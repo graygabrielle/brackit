ec2f7300252e37a5a044914d8492bd7365031008
Cameron <cameronjmaccabe@gmail.com>
2019-04-08 16:54:54 -0700

    added post routes

---
+++ app/controllers/apiController/bracket-controller.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const router = express.Router();
+
+// Import the model (bracket.js) to use its database functions.
+const Bracket = require("../../models/brackets");
+
+// Create all our routes and set up logic within those routes where required.
+
+router.post("/admin", async function(req, res) {
+  try {
+    const newBracket = await req.body;
+    Bracket.create({
+      name: newBracket.bracketName,
+      numberCandidates: newBracket.numberCandidates
+    });
+    res.json(newBracket);
+  } catch(e) {
+    res.send(e);
+  }
+});
+
+// Export routes for server.js to use.
+module.exports = router;
