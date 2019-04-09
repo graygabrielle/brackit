b397f52b2a7b471b458a7095e0741b8cfff68e16
graygabrielle <gray.atkinson.adams@gmail.com>
2019-04-08 22:14:14 -0700

    initialized a partials folder, starting to add basics into views

--- server.js
+++ server.js
@@ -12,14 +12,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //serves static versions of css & js so we can store as seperate files
-app.use(express.static(path.join(__dirname + "app/public")));
+app.use(express.static(path.join(__dirname, "app/public")));
 
 //sets up handlebars
 app.engine("handlebars", handlebars({ defaultLayout: "main"}));
 app.set("view engine", "handlebars");
+//sets view folder for handlebars
+app.set("views", path.join(__dirname, "app/views"));
 
 //points app to routes
 app.use(routes);
+
+app.get('/', function(req, res){
+    res.render('index');
+})
 
 console.log(`log base 2 of 4 is ${Math.log(4, 2)}`);
 
