6b27905e173ab335a281ba2033b22c2a8f978e37
thirty potatoes <drinkereliza@gmail.com>
2019-04-09 23:31:07 -0700

    added groundwork for timer function

--- app/controllers/apiController/admin-controller.js
+++ app/controllers/apiController/admin-controller.js
@@ -3,25 +3,28 @@ const db = require("../../models");
 
 // Create all our routes and set up logic within those routes where required.
 
-router.post("/admins", async function(req, res) {
+router.post("/admins", async function (req, res) {
   try {
-    const newAdmin = await req.body;
-    db.Admin.create({
-      displayName: newAdmin.displayName,
-    }).then(function(response) {
-      res.json(response);
-    })
-  } catch(e) {
+    const response = await db.Admin.create({
+      displayName: req.body.displayName,
+    });
+    res.json(response);
+  } catch (e) {
     res.send(e);
   }
 });
 
-router.get("/admins/:adminCode/:brackitID", function(req, res) {
+router.get("/admins/:adminCode/:brackitID", function (req, res) {
   try {
-    const adminCode = req.params.adminCode;
-    const brackitID = req.params.brackitID;
-    res.render("admin-brackit-codes", {adminCode, brackitID});
-  } catch(e) {
+    const {
+      adminCode,
+      brackitID
+    } = req.params;
+    res.render("admin-brackit-codes", {
+      adminCode: adminCode,
+      brackitID: brackitID
+    });
+  } catch (e) {
     res.send(e);
   }
 })
